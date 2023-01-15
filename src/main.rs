f1adf508494b8370b868c0efc77a23a4397c9b1b
OakmanByte <antonskebaber@gmail.com>
2023-01-15 22:42:29 +0100

    using url instead of string and some refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use ini::Ini;
 use serde_json::json;
+use url::Url;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Repo {
@@ -20,15 +21,21 @@ struct Repo {
     updated_at: String,
 }
 
-fn fetch_repositories(_user_name: &str, access_token: &str) -> Result<Vec<Repo>, reqwest::Error> {
+fn fetch_repositories(_user_name: &str, access_token: &str, include_archived: bool) -> Result<Vec<Repo>, Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::new();
-    let url = format!("https://api.github.com/user/repos");
+    let url = Url::parse("https://api.github.com/user/repos")?;
     let response = client
-        .get(&url)
+        .get(url)
         .header("Authorization", format!("token {}", access_token))
         .header("User-Agent", "conspectus/1.0")
         .send()?;
-    let repos: Vec<Repo> = response.json()?;
+    let mut repos: Vec<Repo> = response.json()?;
+
+    //Remove archived repositories if they shouldn't be included
+    // Can't write !repo.archived for some reason, must be syntax specific
+    if !include_archived {
+        repos.retain(|repo| repo.archived == false)
+    }
     Ok(repos)
 }
 
@@ -54,26 +61,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //Read and parse CONFIG ini file
     let config = Ini::load_from_file("config.ini")?;
-    let section = config.section(Some("Github")).ok_or("Failed to find Github section in config file")?;
-    let user_name = section.get("name").ok_or("Failed to find user_name in config file")?;
-    let access_token = section.get("token").ok_or("Failed to find access_token in config file")?;
+    let section = config.section(Some("Github")).ok_or_else(|| "Failed to find Github section in config file")?;
+    let user_name = section.get("name").ok_or_else(|| "Failed to find user_name in config file")?;
+    let access_token = section.get("token").ok_or_else(|| "Failed to find access_token in config file")?;
 
     if user_name.is_empty() || access_token.is_empty() {
         return Err(From::from("Missing username or access_token in the config file. Please see Readme file on how to setup the config file correctly."));
     }
 
-    let repositories = fetch_repositories(user_name, access_token)?;
+    let mut repositories = fetch_repositories(user_name, access_token, false)?;
 
-    //Filter out archived repositories and format the date string
-    let repositories: Vec<Repo> = repositories
-        .into_iter()
-        .filter_map(|mut repo| {
-            if !repo.archived {
-                repo.updated_at = repo.updated_at.split("T").next().unwrap_or("").to_string();
-                Some(repo)
-            } else { None }
-        })
-        .collect();
+    //Change the datetime string format
+    for repo in &mut repositories {
+        repo.updated_at = repo.updated_at.split("T").next().unwrap_or("").to_string();
+    }
 
     println!("{:#?}", repositories);
 
