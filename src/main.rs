354e6905c67dccfb4d947820e57a30ea29dc8d7d
OakmanByte <antonskebaber@gmail.com>
2023-01-15 16:41:10 +0100

    small changes

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,12 @@ struct Repo {
     updated_at: String,
 }
 
-fn fetch_repositories(user_name: &str, _access_token: &str) -> Result<Vec<Repo>, reqwest::Error> {
+fn fetch_repositories(_user_name: &str, access_token: &str) -> Result<Vec<Repo>, reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let url = format!("https://api.github.com/users/{}/repos", user_name);
+    let url = format!("https://api.github.com/user/repos");
     let response = client
         .get(&url)
-        //.header("Authorization", format!("token {}", access_token))
+        .header("Authorization", format!("token {}", access_token))
         .header("User-Agent", "conspectus/1.0")
         .send()?;
     let repos: Vec<Repo> = response.json()?;
@@ -64,15 +64,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let repositories = fetch_repositories(user_name, access_token)?;
 
+    //Filter out archived repositories and format the date string
     let repositories: Vec<Repo> = repositories
         .into_iter()
-        .map(|mut repo| {
-            repo.updated_at = repo.updated_at.split("T").next().unwrap_or("").to_string();
-            repo
+        .filter_map(|mut repo| {
+            if !repo.archived {
+                repo.updated_at = repo.updated_at.split("T").next().unwrap_or("").to_string();
+                Some(repo)
+            } else { None }
         })
         .collect();
 
-    println!("{:?}", repositories);
+    println!("{:#?}", repositories);
 
     generate_report(user_name, repositories)
 }
